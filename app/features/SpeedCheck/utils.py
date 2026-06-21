145da7e0f6bbe497bacfe45973c50a2a7f76817d
HansakaV <maheshhansaka628@gmail.com>
2026-06-21 20:59:32 +0530

    refactor:make network detactor async and installed required library httpx

--- app/features/SpeedCheck/utils.py
+++ app/features/SpeedCheck/utils.py
@@ -1,6 +1,8 @@
 import json
 import urllib.request
+from fastapi import Request
 from app.core.config import settings
+import httpx
 
 #data generator
 def dummy_data_generator(size_in_mb: int):
@@ -8,24 +10,34 @@ def dummy_data_generator(size_in_mb: int):
     for _ in range(size_in_mb):
         yield chunk_1mb
 
+# Get client IP (resolving localhost/local dev environment and proxy headers)
+def get_client_ip(request: Request) -> str:
+    x_forwarded_for = request.headers.get("X-Forwarded-For")
+    if x_forwarded_for:
+        client_host = x_forwarded_for.split(",")[0].strip()
+    else:
+        client_host = request.client.host if request.client else None
+
+    if not client_host or client_host in ["127.0.0.1", "localhost", "::1"]:
+        return settings.TEST_SERVER_IP
+    return client_host
+
 #Network detector
-def detect_network_info(client_ip: str) -> dict:
-    if client_ip in ["127.0.0.1", "localhost", "::1"]:
-        client_ip = settings.TEST_SERVER_URL   
-
+async def detect_network_info(client_ip: str) -> dict:
     try:
-        url = settings.IP_API_URL + "json/" + client_ip + "?fields=status,message,query,country,city,lat,lon,isp,org,as"
-        req = urllib.request.Request(url,headers={"User-Agent": "Mozilla/5.0"})
+        url = settings.IP_API_URL + "json/" + client_ip + "?fields=status,message,query,country,city,regionName,lat,lon,isp,org,as"
 
-        with urllib.request.urlopen(req, timeout=4) as response:
-            data = json.loads(response.read().decode("utf-8"))
-            
-            if data.get("status") == "success":
-                return{
-                    "ip":data.get("query", client_ip),
-                    "isp":data.get("isp","Unknown"),
-                    "location":f"{data.get('city','Unknown')},{data.get('regionName','Unknown')}, {data.get('country','Unknown')}",
-                }
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, headers={"User-Agent":"Mozilla/5.0"}, timeout=3.0)
+
+            if response.status_code == 200:
+                data = response.json()
+                if data.get("status") == "success":
+                    return{
+                        "ip":data.get("query", client_ip),
+                        "isp":data.get("isp","Unknown"),
+                        "location":f"{data.get('city','Unknown')},{data.get('regionName','Unknown')}, {data.get('country','Unknown')}",
+                    }
 
     except Exception as e:
         return {
