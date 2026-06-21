621dfd929f176ffc2a25187fbf491508a9f8aa31
HansakaV <maheshhansaka628@gmail.com>
2026-06-21 19:39:42 +0530

    refactor:validation updated

--- app/core/config.py
+++ app/core/config.py
@@ -2,17 +2,19 @@ from pydantic_settings import BaseSettings,SettingsConfigDict
 from pydantic import Field
 
 class Settings(BaseSettings):
-    APP_NAME:str = "Check-My-Speed-Engine"
-    APP_VERSION:str = "1.0.0"
-    DEBUG_MODE:bool = True
-    DEFAULT_DOWNLOAD_CHUNK_MB:int = 10
+    APP_NAME:str 
+    APP_VERSION:str 
+    DEBUG_MODE:bool 
+    DEFAULT_DOWNLOAD_CHUNK_MB:int 
+    TEST_SERVER_IP:str
+    IP_API_URL:str 
 
     model_config = SettingsConfigDict(
         env_file = ".env",
         env_file_encoding = "utf-8",
         extra = "ignore",   
     )
-    
+
 settings = Settings()   
 
     
