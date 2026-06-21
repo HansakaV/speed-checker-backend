da5d591e5fed6ead0e90622e878e60988af2bcd9
HansakaV <maheshhansaka628@gmail.com>
2026-06-21 13:41:25 +0530

    feat:upload speed test endpoint developed

--- app/features/SpeedCheck/router.py
+++ app/features/SpeedCheck/router.py
@@ -17,3 +17,10 @@ async def download_speed_test(size_in_mb: int = settings.DEFAULT_DOWNLOAD_CHUNK_
         media_type="application/octet-stream"
     )   
 
+#upload Speed Test Endpoint
+router.post("/upload",status_code=status.HTTP_200_OK)
+async def upload_speed_test(request:Request):
+    body_bytes = 0
+    async for chunk in request.stream():
+        body_bytes += len(chunk)
+          
